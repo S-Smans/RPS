e4beb392a53c28215ba44cc431c52189be0439de
Sandis <smanssandis@gmail.com>
2022-01-01 15:45:38 +0200

    Created pseudo code

---
+++ script.js
@@ -0,0 +1,15 @@
+// create a function computerPlay that randomly returns 'rock', 'paper', 'scissors'.
+// put the 3 available choices in an array
+// call a math.floor function that gets a number from math.random * arrays.length.
+// return the randomly picked choice
+
+// create a variable which stores playerInput that is case-insensitive
+
+// create a function that takes two parameters - players selection and computers selection, and returns a string that declares the winner.
+// create 9 if else checks to check who the winner is 
+// increment the win count by 1 to whoever won the round
+// return string "You Win! ......" or "You Lose! ......" 
+
+// create a function called game() that keeps score and reports a winner or loser at the end of 5 games
+// create two variables that keep player and computer score
+// afer 5 rounds check who the winner is
