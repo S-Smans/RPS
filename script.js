f378116bc68af2eb93b827ed1e0c8d369a560851
Sandis <smanssandis@gmail.com>
2022-01-05 18:41:19 +0200

    Added comments

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
-// create a function computerPlay that randomly returns 'rock', 'paper', 'scissors'.
-// put the 3 available choices in an array
-// call a math.floor function that gets a number from math.random * arrays.length.
-// return the randomly picked choice
-
-// create a variable which stores playerInput that is case-insensitive
-
-// create a function that takes two parameters - players selection and computers selection, and returns a string that declares the winner.
-// create if else checks to check who the winner is 
-// increment the win count by 1 to whoever won the round
-// return string "You Win! ......" or "You Lose! ......" 
-
-// create a function called game() that keeps score and reports a winner or loser at the end of 5 games
-// create two variables that keep player and computer score
-// afer 5 rounds check who the winner is
-
+// function that randomly return 'rock', 'paper' or 'scissors'
 function computerPlay() {
     let choice = ['rock', 'paper', 'scissors'];
     let number = Math.floor(Math.random() * choice.length);
     return choice[number];
 }
 
+// main game function that keeps score of 5 games
 function game() {
+
+    //  function checks who the winner is and returns a string that declares the winner
     function playRound(playerSelection, computerSelection) {
 
         if (playerSelection == computerSelection) {
@@ -52,11 +40,13 @@ function game() {
     let computerScore = 0;
     let finishedGames = 0;
 
+    // game loop for 5 finished games
     while (finishedGames < 5) {
         
         let playerInput = prompt('You\'re playing against a computer.\nPick rock, paper or scissors?').toLowerCase();
         let result = playRound(playerInput, computerPlay());
 
+        // if false game doesn't count as finished
         if (result === false) {
             console.log('Incorrect input. Try again.');
             continue;
@@ -66,8 +56,10 @@ function game() {
         finishedGames++;
     }
 
+    // Logs out the games score
     console.log(`Score: Player ${playerScore}, Computer ${computerScore}`);
     
+    // Logs out who the winner is
     if (playerScore > computerScore) {
         console.log('Congratulations! You Won!');
     } else if (computerScore > playerScore) {
